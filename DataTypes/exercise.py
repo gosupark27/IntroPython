7b9077cfca3ee7b6e5ff23d2c1f98653ca3d80e9
gosupark27 <hpark41@wgu.edu>
2024-09-27 20:32:24 -0700

    completed exercise for data types

---
+++ DataTypes/exercise.py
@@ -0,0 +1,48 @@
+# Question 1 - Identify data type/class for each following values
+
+# str or String
+'True'
+
+# bool or Boolean
+False
+
+# tuple
+(1, 2, 3) 
+
+# float
+1.5
+
+# list
+[1, 2, 3]
+
+# int or Integer
+2
+
+# range
+range(5) 
+
+# set
+{1, 2, 3}
+
+# NoneType
+None
+
+# dict or dictionary 
+{'foo': 'bar'}
+
+# Question 2 - Create a tuple called names that contain several pet names 
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+# Question 3 - Create a dictionary named pets that contains a list of pet names and type of animal
+
+pets = {
+    names[0]: 'dog',
+    names[1]: 'cat',
+    names[2]: 'cat',
+    names[3]: 'fish',
+    names[4]: 'lizard',
+}
+
+print(pets)
+
+
