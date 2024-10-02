ce7bdcee9ef862ff94d717bdd9ccd9ea057cbd4b
gosupark27 <hpark41@wgu.edu>
2024-10-01 22:06:44 -0700

    exercise 1 saved changes

---
+++ LoopsIterating/exercise1.py
@@ -0,0 +1,7 @@
+# This is an infinite loop because counter is never incremented after each iteration 
+# So, the while conditional will never terminate 
+
+counter = 0
+
+while counter < 5:
+    print(counter)
